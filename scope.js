7cfe8afc6963dc9f24b64ea9efe304d4a4c97ec7
Mark <jungtaehoon@icloud.com>
2017-05-01 16:40:29 +0900

    change scope answer

--- scope.js
+++ scope.js
@@ -5,4 +5,6 @@ function logIt(){
 };
 logIt();
 
-//I think it will console.log undefined
+// I think this will console.log undefined
+// It is because of hoisting(hoisting is a just a concept not real thing!)
+
