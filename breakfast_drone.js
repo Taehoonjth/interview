431cc9a638bad596d495877e3fd911eb0c5f1d3a
Mark <jungtaehoon@icloud.com>
2017-06-05 16:55:36 +0900

    working on breakfast drone

--- breakfast_drone.js
+++ breakfast_drone.js
@@ -12,4 +12,34 @@ function findMissingId(array) {
   return Object.keys(result)[0];
 }
 
-console.log(findMissingId(deliveryIdConfirmations))
+// console.log(findMissingId(deliveryIdConfirmations))
+//use Map
+function findMissingId2(array) {
+  var map = new Map();
+  deliveryIdConfirmations.forEach(element => {
+    if (map.has(element)) {
+      map.delete(element);
+    } else {
+      map.set(element, 0);
+    }
+  });
+  return map.keys().next().value;
+}
+
+function findMissingId3(array) {
+  var map = new Map();
+
+}
+
+  function findUniqueDeliveryId4(deliveryIds) {
+
+    var uniqueDeliveryId = 0;
+
+    deliveryIds.forEach(function(deliveryId) {
+        uniqueDeliveryId ^= deliveryId;
+        console.log(uniqueDeliveryId.toString(2))
+    });
+
+    return uniqueDeliveryId;
+}
+console.log(findUniqueDeliveryId4(deliveryIdConfirmations));
